46eea4616c3d6dd66550bc504aadbf1908b960d0
Lien Zhu <lienzhucs@gmail.com>
2022-05-26 23:28:32 -0500

    playing with grid

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 // script for personal portfolio website
 //
 
+        
+
+const colorArr = 
+    [   "#ffe74cff",
+        "#ff5964ff",
+        "#FAF7F7FF",
+        "#0a0908ff",
+        "#35a7ffff"     ]
+
