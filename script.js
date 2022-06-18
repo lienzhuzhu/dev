eaa99d76598dec73c8a07d93c1d84e2c9ca95c43
Lien Zhu <lienzhucs@gmail.com>
2022-06-18 00:21:43 -0500

    use flexbox and maybe react?

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 // script for personal portfolio website
 //
 
-        
-
-const colorArr = 
-    [   "#ffe74cff",
-        "#ff5964ff",
-        "#FAF7F7FF",
-        "#0a0908ff",
-        "#35a7ffff"     ]
-
