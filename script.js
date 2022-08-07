1758b2d62b0fb6ad52c5309348a196da32b82fa7
Lien Zhu <lienzhucs@gmail.com>
2022-08-06 19:42:01 -0500

    added modal

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ let homeBtn = document.getElementById("back-to-home-button");
 homeBtn.onclick = () => {
     document.getElementById("landing").scrollIntoView();
 }
+
+let showMessage = true;
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (showMessage) {
+        document.getElementById("modal").style.display = "flex";
+    } else {
+        document.getElementById("modal").style.display = "none";
+    }
+});
+
+document.getElementById("modal").addEventListener("click", closeModal);
+// window.addEventListener("click", closeModal);
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
